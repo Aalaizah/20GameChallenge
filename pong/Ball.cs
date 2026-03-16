8c9d291914297cd86dd0dbe9914a03dc12477885
Alexandria Mack <amm4108@gmail.com>
2026-03-16 13:56:43 -0500

    ball now moves and collides, score can be gained. Still needs to reset after scoring

--- pong/Ball.cs
+++ pong/Ball.cs
@@ -1,27 +1,53 @@
 using Godot;
 
-public partial class Ball : Node2D
+public partial class Ball : RigidBody2D
 {
-    private int _width;
-    private int _xLoc;
-    private int _yLoc;
-    private int _speed;
+	private int _width;
+	private int _speed;
 
-    public override void _Ready()
-    {
-        var screenSize = GetViewportRect().Size;
-        _width = (int)screenSize.Y;
-        var random = new RandomNumberGenerator();
-        _speed = random.RandiRange(1, 5);
-    }
+	public override void _Ready()
+	{
+		var screenSize = GetViewportRect().Size;
+		_width = (int)screenSize.X;
+		GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D").ScreenExited += Score;
+	}
 
-    public override void _Process(double delta)
-    {
-        
-    }
+	public void SpawnBall(Vector2 position, float direction, Vector2 velocity)
+	{
+		Rotation = direction;
+		Position = position;
+		SetLinearVelocity(velocity.Rotated(direction));
+	}
 
-    public void Bounce()
-    {
-        
-    }
+	public override void _Process(double delta)
+	{
+		var collision = MoveAndCollide(GetLinearVelocity() * (float)delta);
+		if (collision != null)
+		{
+			SetLinearVelocity(GetLinearVelocity().Bounce(collision.GetNormal()));
+		}
+	}
+
+	public void Score()
+	{
+		if (this.Position.X > _width)
+		{
+			GD.Print("p1 scored");
+			GetNode<Signals>("/root/Signals").EmitSignal("PlayerScored", 1);
+		}
+		if (this.Position.X < 0)
+		{
+			GD.Print("p2 scored");
+			GetNode<Signals>("/root/Signals").EmitSignal("PlayerScored", 2);
+		}
+		
+		if (Input.IsActionPressed("p1ScoreTest"))
+		{
+			GetNode<Signals>("/root/Signals").EmitSignal("PlayerScored", 1);
+		}
+		if (Input.IsActionPressed("p2ScoreTest"))
+		{
+			GetNode<Signals>("/root/Signals").EmitSignal("PlayerScored", 2);
+		}
+	}
 }
--- pong/Main.cs
+++ pong/Main.cs
@@ -9,32 +9,62 @@ public partial class Main : Node2D
      * res://paddle.tscn
      */
     private PackedScene _paddleScene = ResourceLoader.Load<PackedScene>("res://paddle.tscn");
+    private PackedScene _ballScene = ResourceLoader.Load<PackedScene>("res://ball.tscn");
     private Paddle _player1Paddle;
     private Paddle _player2Paddle;
-    private int _player1Score;
-    private int _player2Score;
+    private int[] _scores = [0, 0];
 
     public override void _Ready()
     { 
-	    _player1Paddle = _paddleScene.Instantiate<Paddle>();
-	    _player1Paddle.Name = "player1";
-	    _player1Paddle.SetP1XLoc(10);
-	    _player1Paddle.Position = new Vector2(10, 50);
-		AddChild(_player1Paddle);
+	    _NewGame();
+    }
 
-		var screenWidth = GetViewportRect().End;
-		var p2XLoc = screenWidth.X - 50;
-		_player2Paddle = _paddleScene.Instantiate<Paddle>();
-		_player2Paddle.Name = "player2";
-		_player2Paddle.SetPlayer(2);
-		_player2Paddle.SetP2XLoc((int)p2XLoc);
-		_player2Paddle.Position = new Vector2(p2XLoc, 50);
-		AddChild(_player2Paddle);
+    private void _PlayerScored(int player)
+    {
+	    switch (player)
+	    {
+		    case 1: 
+			    _scores[0] += 1;
+			    GetNode<Label>("Arena/P1 Score").Text = _scores[0].ToString();
+			    break;
+		    case 2: _scores[1] += 1;
+			    GetNode<Label>("Arena/P2 Score").Text = _scores[1].ToString();
+			    break;
+	    }
     }
 
     private void _NewGame()
     {
+	    _player1Paddle = _paddleScene.Instantiate<Paddle>();
+	    _player1Paddle.Name = "player1";
+	    _player1Paddle.SetP1XLoc(10);
+	    _player1Paddle.Position = new Vector2(10, 50);
+	    AddChild(_player1Paddle);
+
+	    var screenWidth = GetViewportRect().End;
+	    var p2XLoc = screenWidth.X - 50;
+	    _player2Paddle = _paddleScene.Instantiate<Paddle>();
+	    _player2Paddle.Name = "player2";
+	    _player2Paddle.SetPlayer(2);
+	    _player2Paddle.SetP2XLoc((int)p2XLoc);
+	    _player2Paddle.Position = new Vector2(p2XLoc, 50);
+	    AddChild(_player2Paddle);
+	    GetNode<Signals>("/root/Signals").PlayerScored += this._PlayerScored;
+	    _SpawnBall();
 	    
+    }
+
+    private void _SpawnBall()
+    {
+	    Ball ball = _ballScene.Instantiate<Ball>();
+	    var viewportRect = GetViewportRect().Size;
+	    var screenWidth = viewportRect.X;
+	    var screenHeight = viewportRect.Y;
+	    var ballSpawn = new Vector2(screenWidth / 2, screenHeight / 2);
+	    float direction = (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+	    var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+	    ball.SpawnBall(ballSpawn, direction, velocity);
+	    AddChild(ball);
     }
 
     private void _GameOver()
